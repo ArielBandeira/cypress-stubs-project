a77840603922c88fbd21ed4fd3213e06dd2b37d5
arieltwain <arieltwain@gmail.com>
2023-05-29 23:47:01 -0300

    testing snapLocation page

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,8 +1,59 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
+  beforeEach(() => {
+    cy.clock()
+    cy.fixture('user-location.json').as('userLocation')
+    cy.visit('/').then((win) => {
+      cy.get('@userLocation').then(fakePosition => {
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+        .as('getUserPosition')
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb(fakePosition)
+          }, 100)
+        })
+      })
+      cy.stub(win.navigator.clipboard, 'writeText')
+        .as('saveToClipboard')
+        .resolves()
+      cy.spy(win.localStorage, 'setItem').as('storeLocation')
+      cy.spy(win.localStorage, 'getItem').as('getStoredLocation')
+    })
+  })
   it('should fetch the user location', () => {
-    cy.visit('/');
-    cy.get('[data-cy="get-loc-btn"]').click();
-  });
-});
+    cy.get('#get-location').as('getLocBtn')
+    cy.get('@getLocBtn').click()
+    cy.get('@getUserPosition').should('have.been.called')
+    cy.get('@getLocBtn').should('be.disabled')
+    cy.get('.actions').should('contain', 'Location fetched')
+  })
+
+  it('should share a location URL', () => {
+    cy.get('#name').type('John Doe')
+    cy.get('#get-location').as('getLocBtn')
+    cy.get('@getLocBtn').click()
+    cy.get('@getLocBtn').should('be.disabled')
+    cy.get('.actions').should('contain', 'Location fetched')
+    cy.get('[type="submit"]').click()
+    cy.get('@saveToClipboard').should('have.been.called')
+    cy.get('#info-message').should('be.visible')
+    cy.get('@userLocation').then((fakePosition) => {
+      const { latitude, longitude } = fakePosition.coords
+      cy.get('@saveToClipboard').should(
+        'have.been.calledWithMatch', 
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`)
+      )
+      cy.get('@storeLocation').should(
+        'have.been.calledWithMatch',
+        /John Doe/,
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`)
+      )
+    })
+    cy.get('[type="submit"]').click()
+    cy.get('@getStoredLocation').should('have.been.called')
+    cy.get('#info-message').should('have.class', 'visible')
+    cy.tick(2000)
+    cy.get('#info-message').should('not.be.visible')
+  })
+})
